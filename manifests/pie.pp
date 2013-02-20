c5bda2bde31f41d300dd5e1551fd778721dbe936
wilas <limak.kamwil@gmail.com>
2013-02-21 00:03:59 +0100

    rename yum repos module, add adm property

--- manifests/pie.pp
+++ manifests/pie.pp
@@ -1,10 +1,10 @@
 stage { "base": before  => Stage["main"] }
 stage { "last": require => Stage["main"] }
 
-class { "install_repos": stage => "base" }
+class { "yum_repos": stage     => "base" }
 class { "basic_package": stage => "base" }
 class { "user::root": stage    => "base" }
-Class["install_repos"] -> Class["basic_package"] -> Class["user::root"]
+Class["yum_repos"] -> Class["basic_package"] -> Class["user::root"]
 
 class { "user::virtual": }
 class { "user::horde_air": }
